3b1c72b3c003ccb955dfd52da82717140ab9c085
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-08-31 10:00:55 -0400

    add url validation

--- src/hooks/useMCPFetch.tsx
+++ src/hooks/useMCPFetch.tsx
@@ -2,6 +2,11 @@ const useMCPFetch = (ipAddress: string) => {
     const mcpGet = (url: string, timeout: number = 10) => {
         if (!ipAddress)
             throw new Error(`Attempt to make a GET request to the MCP Server without setting an IP Address.`);
+
+        if (typeof url !== "string" || !url.startsWith("/")) {
+            throw new Error(`Invalid URL path: ${url}. URL must start with /`);
+        }
+
         return fetch(`http://${ipAddress}${url}`, {
             method: "GET",
             headers: { "Content-Type": "application/json" },
@@ -12,6 +17,11 @@ const useMCPFetch = (ipAddress: string) => {
     const mcpPost = (url: string, body: BodyInit, timeout: number = 10) => {
         if (!ipAddress)
             throw new Error(`Attempt to make a POST request to the MCP Server without setting an IP Address.`);
+
+        if (typeof url !== "string" || !url.startsWith("/")) {
+            throw new Error(`Invalid URL path: ${url}. URL must start with /`);
+        }
+
         return fetch(`http://${ipAddress}${url}`, {
             method: "POST",
             body: body,
